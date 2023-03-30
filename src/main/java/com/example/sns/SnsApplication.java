e1721d9c89f76bc7d1f621facf1effa6296a2368
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-30 16:58:59 +0900

    [Feat] 알람 기능 트랜잭션 분리, 비동기 통신

--- src/main/java/com/example/sns/SnsApplication.java
+++ src/main/java/com/example/sns/SnsApplication.java
@@ -3,9 +3,11 @@ package com.example.sns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableAsync
 public class SnsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/sns/event/handler/AlarmEventHandler.java
+++ src/main/java/com/example/sns/event/handler/AlarmEventHandler.java
@@ -2,8 +2,10 @@ package com.example.sns.event.handler;
 
 import com.example.sns.event.AlarmEvent;
 import com.example.sns.repository.AlarmRepository;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class AlarmEventHandler {
@@ -13,8 +15,10 @@ public class AlarmEventHandler {
         this.alarmRepository = alarmRepository;
     }
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
     public void createAlarm(AlarmEvent event) {
         alarmRepository.save(event.getAlarm());
     }
 }
+
